4efd5b5f448d85aae797cbca2d1697cf05945a16
Christian Dähn <christian.daehn@gmail.com>
2016-08-10 20:13:12 +0200

    countdown pauseable

---
+++ app/src/androidTest/java/com/muckibude/cda/lightningtalktimer/BackCountdownDisplayTest.java
@@ -0,0 +1,52 @@
+package com.muckibude.cda.lightningtalktimer;
+
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.muckibude.cda.lightningtalktimer.matcher.ImageResourceMatcher.withDrawable;
+
+@RunWith(AndroidJUnit4.class)
+public class BackCountdownDisplayTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test(expected = NoMatchingViewException.class)
+    public void backFragmentBigNumberViewNotVisibleOnStart() {
+        onView(withId(R.id.big_number_text_view)).check(matches(isDisplayed()));
+    }
+
+    @Test(expected = NoMatchingViewException.class)
+    public void backFragmentSmallNumberViewNotVisibleOnStart() {
+        onView(withId(R.id.small_number_text_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void countdownDisplayShownWhenStartButtonPressed() {
+        onView(withId(R.id.minutes)).check(matches(withText("2")));
+        onView(withId(R.id.seconds)).check(matches(withText("15")));
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withId(R.id.big_number_text_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.small_number_text_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onResumeTimerWillResume() {
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withId(R.id.pauseButton)).check(matches(withDrawable(R.drawable.pause_overlay)));
+        onView(withId(R.id.pauseButton)).perform(click());
+        onView(withId(R.id.pauseButton)).check(matches(withDrawable(R.drawable.play_overlay_icon)));
+    }
+
+}
