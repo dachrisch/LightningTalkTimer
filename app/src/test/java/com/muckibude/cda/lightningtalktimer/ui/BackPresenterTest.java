2af946c2720c34dbdb160d32e67a2cfbc74eb98e
Christian Dähn <christian.daehn@gmail.com>
2016-08-18 22:11:09 +0200

    inc and dec to the next 15 seconds

--- app/src/test/java/com/muckibude/cda/lightningtalktimer/ui/BackPresenterTest.java
+++ app/src/test/java/com/muckibude/cda/lightningtalktimer/ui/BackPresenterTest.java
@@ -16,37 +16,28 @@ public class BackPresenterTest {
     @Test
     public void displayMinutesBigIfAboveOneMinute() {
         BackView backView = mock(BackView.class);
-
         PauseableMinutesSecondsTimer timer = mock(PauseableMinutesSecondsTimer.class);
-
         BackPresenter backPresenter = new BackPresenter(new BackModel(new MinutesSecondsEntity(1, 1)), timer);
         backPresenter.setView(backView);
         backPresenter.onSecond();
-
         verify(backView).display(1, 0);
     }
 
     @Test
     public void displaySecondsBigIfBelowOneMinute() {
         BackView backView = mock(BackView.class);
-
         PauseableMinutesSecondsTimer timer = mock(PauseableMinutesSecondsTimer.class);
-
         BackPresenter backPresenter = new BackPresenter(new BackModel(new MinutesSecondsEntity(1, 0)), timer);
         backPresenter.setView(backView);
         backPresenter.onSecond();
-
         verify(backView).display(59);
     }
 
     @Test
     public void cancelTimerWhenStopTimer() {
         PauseableMinutesSecondsTimer timer = mock(PauseableMinutesSecondsTimer.class);
-
         BackPresenter backPresenter = new BackPresenter(new BackModel(new MinutesSecondsEntity(1, 0)), timer);
-
         backPresenter.stopTimer();
-
         verify(timer).cancel();
     }
 
@@ -57,7 +48,6 @@ public class BackPresenterTest {
         fragment.backPresenter = new BackPresenter(new BackModel(new MinutesSecondsEntity(1, 0)), timer);
         fragment.onDestroyView();
         verify(timer).cancel();
-
     }
 
 
