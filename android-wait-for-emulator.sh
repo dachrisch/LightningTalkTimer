bab27d375ea506280b4c8b2e7558d55d938b83b3
Christian Dähn <christian.daehn@gmail.com>
2016-08-28 19:59:48 +0200

    wait for package service come online

--- android-wait-for-emulator.sh
+++ android-wait-for-emulator.sh
@@ -3,11 +3,12 @@
 # Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
 # Source: https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
 
+echo -n "checking for device start..."
 bootanim=""
 failcounter=0
 until [[ "$bootanim" =~ "running" ]]; do
    bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
-   echo "$bootanim"
+   echo -n "."
    if [[ "$bootanim" =~ "error" ]]; then
       let "failcounter += 1"
       if [[ $failcounter -gt ${WAIT_EMULATOR_MAX_RETRIES} ]]; then
@@ -17,6 +18,21 @@ until [[ "$bootanim" =~ "running" ]]; do
    fi
    sleep ${WAIT_EMULATOR_TIME}
 done
-echo "Done"
+echo "started!"
 
-set -e
+echo -n "checking for package service..."
+service_check=""
+failcounter=0
+until [[ "$service_check" = "Service package: found" ]]; do
+    service_check=`adb shell service check package`
+    echo -n "."
+    if [[ "$service_check" =~ "not found" ]]; then
+      let "failcounter += 1"
+      if [[ $failcounter -gt ${WAIT_EMULATOR_MAX_RETRIES} ]]; then
+        echo "Failed to start emulator"
+        exit 1
+      fi
+    fi
+    sleep ${WAIT_EMULATOR_TIME}
+done
+echo "found!"
