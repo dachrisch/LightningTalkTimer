7cf9055f2484558ac8f452094cb5d5f29b6a1ef6
Christian Dähn <christian.daehn@gmail.com>
2016-09-06 22:53:23 +0200

    moved blinking to current thread

--- app/src/main/java/com/muckibude/cda/lightningtalktimer/presentation/BackPresenter.java
+++ app/src/main/java/com/muckibude/cda/lightningtalktimer/presentation/BackPresenter.java
@@ -1,24 +1,18 @@
 package com.muckibude.cda.lightningtalktimer.presentation;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.muckibude.cda.lightningtalktimer.data.CountdownEntity;
 import com.muckibude.cda.lightningtalktimer.data.EntityChangeListener;
 import com.muckibude.cda.lightningtalktimer.domain.BackModel;
 import com.muckibude.cda.lightningtalktimer.domain.timer.OnFinishedListener;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import javax.inject.Inject;
 
 public class BackPresenter implements Presenter<BackView>, EntityChangeListener<CountdownEntity>, OnFinishedListener {
     private static final String TAG = "BackPresenter";
     private final BackModel backModel;
-    private final ExecutorService executorService;
     private BackView backView;
-    private boolean isFinished;
 
 
     @Inject
@@ -26,7 +20,6 @@ public class BackPresenter implements Presenter<BackView>, EntityChangeListener<
         this.backModel = backModel;
         this.backModel.registerEntityChangeListener(this);
         this.backModel.registerOnFinishedListener(this);
-        executorService = Executors.newSingleThreadExecutor();
     }
 
     @Override
@@ -35,23 +28,7 @@ public class BackPresenter implements Presenter<BackView>, EntityChangeListener<
     }
 
     public void startTimer() {
-        isFinished = false;
         backModel.startCountdown();
-
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                while (!isFinished) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        Log.d(TAG, "this shouldn't happen: " + e.getLocalizedMessage());
-                        return;
-                    }
-                }
-                backView.blinkScreen();
-            }
-        });
     }
 
     public void toggleTimer() {
@@ -89,6 +66,6 @@ public class BackPresenter implements Presenter<BackView>, EntityChangeListener<
 
     @Override
     public void informFinished() {
-        isFinished = true;
+        backView.blinkScreen();
     }
 }
