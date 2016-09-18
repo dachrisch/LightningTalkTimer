8770ba2eb810ee8e1a5bb1bc958cd2d558246038
Christian Dähn <christian.daehn@gmail.com>
2016-09-18 17:05:24 +0200

    prepare for own CI

--- scripts/build-arm.sh
+++ scripts/build-arm.sh
@@ -2,10 +2,16 @@
 
 export SNAP_CACHE_DIR=/var/services/tmp/
 export ANDROID_HOME=/var/services/homes/cda/dev/android_sdk
+export ANDROID_SWT_JAR=/var/packages/CrashPlan/target/lib/swt.jar
 
 ./download-android.sh
 
-export PATH=$PATH:$ANDROID_HOME/tools:/var/packages/git/target/bin
+if [ ! -d ${ANDROID_HOME}/tools/lib/arm ];then
+    echo -n "copying SWT from CrashPlan..."
+    mkdir ${ANDROID_HOME}/tools/lib/arm
+    cp ${ANDROID_SWT_JAR} ${ANDROID_HOME}/tools/lib/arm
+fi
+
+export PATH=$PATH:${ANDROID_HOME}/tools:/var/packages/git/target/bin
 
 ./initialize-android.sh
-
